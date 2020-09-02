776605a89ef60d8040f1e8d59c4b00d3e18aeee5
Daniel Gould <dmgould310@gmail.com>
2020-09-02 14:04:35 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,9 +4,10 @@ class Show < ActiveRecord::Base
 
   has_many :characters 
   has_many :actors, through: :characters 
+  belongs_to :network 
 
   def actors_list
-    binding.pry 
+    # binding.pry 
   end 
   
   
